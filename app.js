4ec66c78e4c2534bc35c41f77ea940768da1024a
anamika7153 <aanyayadav419@gmail.com>
2021-09-02 20:07:34 +0530

    data types

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 
+//Data types : Primitive and non primitive
+//Primitive Data Types
 
-console.log("This is my first piece of javascript code!!")
-console.log("545434354368464131")
+let age = 21
+console.log(age)
+console.log(typeof age)
+console.log(" ")
 
-console.log(true)
-console.log(true)
-console.log({name:"Anamika"})
-console.warn("This is warning")
-console.error("This is an error")
+var Name = "Anamika"
+let Nam = "Kartik"
+console.log(Name)
+console.log(Nam)
+console.log(typeof Nam)
+console.log(" ")
+
+var charr = '45jgbkmjg'
+console.log(typeof charr)
+console.log(" ")
+let brand=null
+console.log(brand)
+console.log(typeof brand)
+console.log(typeof undefined)
+
+let isTrue=true
+
+//Non-Primitive Data Types
+let arr = [1,2,3,4]
+arr.push("value")
+arr.push(false)
+arr.unshift(false) //adds to beginning of array
+console.log(arr)
+console.log(typeof arr)
