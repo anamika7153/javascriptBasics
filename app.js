f12c8ad054cee1a82272a94a85015bbe3a253d5b
anamika7153 <aanyayadav419@gmail.com>
2021-09-02 19:31:17 +0530

    basics

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+console.log("This is my first piece of javascript code!!")
+console.log("545434354368464131")
+
+console.log(true)
+console.log(true)
+console.log({name:"Anamika"})
+console.warn("This is warning")
+console.error("This is an error")
