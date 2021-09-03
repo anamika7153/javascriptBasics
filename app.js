04f9d6f99764ddbc3d14a82abbce356549be52cd
anamika7153 <aanyayadav419@gmail.com>
2021-09-03 19:49:04 +0530

    Assignment operators

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Arithmetic Operators
 const a= 5
 const b= 15
 console.log(a+b)
@@ -6,3 +7,15 @@ console.log(a*b)
 console.log(a/b)
 console.log(a**b)
 console.log(a%b)
+
+//Concatenation 
+const c= "3"
+const d= 3
+console.log(c-d)
+
+//Assignment Operators
+let n= "Anamika"
+let x = 19
+ x+=n
+ console.log(x)
+ console.log(typeof x)
