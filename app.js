6bcd335871a8eec0784500e74f8fd95d45c4dbad
anamika7153 <aanyayadav419@gmail.com>
2021-09-06 13:55:09 +0530

    DOM selectors

--- app.js
+++ app.js
@@ -1,33 +1,5 @@
-
-//Data types : Primitive and non primitive
-//Primitive Data Types
-
-let age = 21
-console.log(age)
-console.log(typeof age)
-console.log(" ")
-
-var Name = "Anamika"
-let Nam = "Kartik"
-console.log(Name)
-console.log(Nam)
-console.log(typeof Nam)
-console.log(" ")
-
-var charr = '45jgbkmjg'
-console.log(typeof charr)
-console.log(" ")
-let brand=null
-console.log(brand)
-console.log(typeof brand)
-console.log(typeof undefined)
-
-let isTrue=true
-
-//Non-Primitive Data Types
-let arr = [1,2,3,4]
-arr.push("value")
-arr.push(false)
-arr.unshift(false) //adds to beginning of array
-console.log(arr)
-console.log(typeof arr)
+// const element = document.getElementById('selectMe')
+// const element = document.getElementsByClassName('heading')
+// const element = document.getElementsByTagName('div')
+const element = document.querySelectorAll('.heading')
+console.log(element)
