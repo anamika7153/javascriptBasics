a24b193c9902b56ff2d9916c0243151b7e5839e8
anamika7153 <aanyayadav419@gmail.com>
2021-09-06 18:48:01 +0530

    ES-6 Functions

--- app.js
+++ app.js
@@ -5,10 +5,23 @@
 function logName(Name="Enter your name", age=18){
     console.log(Name + " You are " + age)
 }
+
 logName("Anamika")
 
-function add(a=0, b=0) {
+const add = function(a=0, b=0) {      //Another way of declaring functions using variable
     const sum = a+b
     return sum
 }
 console.log(add(12,99))
+
+// const add = (a=0, b=0) => {      //Without keyword function
+//     const sum = a*b
+//     return sum
+// }
+// console.log(add(12,99))
+
+
+
+//ECMA SCRIPT : just a standard on which a javascript is based
+// example : ES-6(version)
+
