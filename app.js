5a89c608fd6b668e69f6d7ffdd3e3619ba0287c1
anamika7153 <aanyayadav419@gmail.com>
2021-09-06 14:48:06 +0530

    node methods

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const element = document.querySelector('#selectMe')
 
-element.innerHTML = "This is from JS"
+// console.log(element.classList)
+// element.classList.add('five')
+// console.log(element.classList)
+// element.classList.remove('three')
+// console.log(element.classList)
 
-element.style.color ="cyan"
-element.style.fontSize ="40px"
-element.style = "display:none"
+// const body = document.querySelector('body')
+// console.log(body.classList.toggle('dark'))
+
+ const value = element.classList.contains('two')
+ console.log(value)
+
