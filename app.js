c2ac5642bf339fe2ff0bcb64fa0a88c631d1306e
anamika7153 <aanyayadav419@gmail.com>
2021-09-07 18:19:37 +0530

    Dom and Array methods

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const balls = document.querySelectorAll('.ball')
 // const balls = document.getElementsByClassName('.ball')
 const body = document.querySelector('body')
-const arr = [1,2,3,4,5]
-// arr.push(6)
-// console.log(arr)
-// while(arr.length<10) {
-//     arr.push("iteration")
-//     added = arr.unshift(41)  //adds to the beginning of array and shift removes the starting element
-//     present = arr.includes(50)
-// }
-// console.log(arr)
-// console.log(arr.length)
-// console.log(added)
-// console.log(present)
 
-// for(var i=0;i<arr.length;i++) {
-//     console.log(arr[i])
-// }
-arr.forEach((ele,iteration) => {
-    console.log(ele,iteration)
+balls.forEach((ball,iteration) => {
+    console.log(iteration)
+    const value =iteration * 50
+    ball.style = `transform:translateY(${value}px)`
 })
