f207fcd735406b1909b0b9a856d32a214be427d1
anamika7153 <aanyayadav419@gmail.com>
2021-09-07 17:33:08 +0530

    For and While Loop

--- app.js
+++ app.js
@@ -1,33 +1,11 @@
 
-//Data types : Primitive and non primitive
-//Primitive Data Types
+// for(var i=1;i<=10;i++) {
+//     var a = "19"
+//     console.log('iteration '+i)
+// }
 
-let age = 21
-console.log(age)
-console.log(typeof age)
-console.log(" ")
-
-var Name = "Anamika"
-let Nam = "Kartik"
-console.log(Name)
-console.log(Nam)
-console.log(typeof Nam)
-console.log(" ")
-
-var charr = '45jgbkmjg'
-console.log(typeof charr)
-console.log(" ")
-let brand=null
-console.log(brand)
-console.log(typeof brand)
-console.log(typeof undefined)
-
-let isTrue=true
-
-//Non-Primitive Data Types
-let arr = [1,2,3,4]
-arr.push("value")
-arr.push(false)
-arr.unshift(false) //adds to beginning of array
-console.log(arr)
-console.log(typeof arr)
+let i = 0
+while(i<5) {
+    console.log(i)
+    i++
+}
