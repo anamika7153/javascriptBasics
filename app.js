0bcfda90dfb91c6724e040e9f36d2127d4c9122b
anamika7153 <aanyayadav419@gmail.com>
2021-09-07 18:14:29 +0530

    Array methods

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-
-// for(var i=1;i<=10;i++) {
-//     var a = "19"
-//     console.log('iteration '+i)
+const balls = document.querySelectorAll('.ball')
+// const balls = document.getElementsByClassName('.ball')
+const body = document.querySelector('body')
+const arr = [1,2,3,4,5]
+// arr.push(6)
+// console.log(arr)
+// while(arr.length<10) {
+//     arr.push("iteration")
+//     added = arr.unshift(41)  //adds to the beginning of array and shift removes the starting element
+//     present = arr.includes(50)
 // }
+// console.log(arr)
+// console.log(arr.length)
+// console.log(added)
+// console.log(present)
 
-let i = 0
-while(i<5) {
-    console.log(i)
-    i++
-}
+// for(var i=0;i<arr.length;i++) {
+//     console.log(arr[i])
+// }
+arr.forEach((ele,iteration) => {
+    console.log(ele,iteration)
+})
