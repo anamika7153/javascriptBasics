16670308c506738566eeb87c58d97f939f002e07
anamika7153 <aanyayadav419@gmail.com>
2021-09-08 20:04:18 +0530

    Object destructuring

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-console.log("hi")
-var arr = [1,2,3,4]
-console.log(arr[2])
-var [one,two,three,four,five]=[1,2,3,4]  //ES6-Destructuring
-console.log(three)
-console.log(four) 
-console.log(five)         //undefined
 
-function getScores() {
-    return [90,100]
+const obj = {
+    nam: "Anamika Yadav",
+    email: "aanyayadav419@gmail.com",
+    age: 20
 }
-let [x,y,z] = getScores();
-console.log(x)
-console.log(y)
-console.log(z)
+console.log(obj.email)
+let {nam,age,email,dumb} = obj
+console.log(email)
+console.log(dumb)
+console.log("==================")
+
+let person = {
+    firstname: "Rini",
+    lastname: "Srivastava",
+    age1: 40,
+    middlename:"abc"
+}
+let {age1,firstname,lastname,middlename=""} = person
+console.log(age1)
+console.log(firstname)
+console.log(middlename)
+console.log(lastname)
