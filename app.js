e25e5436aae3c0b66d4aa422b317c00c6adb0beb
anamika7153 <aanyayadav419@gmail.com>
2021-09-09 20:05:20 +0530

    filter method

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("MERGED CLASS 2 ES6");
 
-let ranks = [1, 2, 3, 4];
+let ranks = [1, 2, 3, 4, 5 ,6];
 
 console.log(
   ranks.map((e, index) => {
@@ -14,10 +14,17 @@ console.log(
   })
 );
 
-let newarr = ranks.map((e, index) => {
+let newmaparr = ranks.map((e, index) => {
     console.log(e)
-    console.log(index)
     if(e%2===0)
         return e
 })
-console.log(newarr);
+console.log(newmaparr);
+
+
+let newfilterarr = ranks.filter((e, index) => {
+    console.log(e)
+    if(e%2===0)
+        return e
+})
+console.log(newfilterarr);
