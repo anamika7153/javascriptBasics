48309d5c8dc223d7913cb002b1096a8942e7a723
anamika7153 <aanyayadav419@gmail.com>
2021-09-09 20:51:05 +0530

    Promise, then & catch

--- app.js
+++ app.js
@@ -1,30 +1,19 @@
-console.log("MERGED CLASS 2 ES6");
-
-let ranks = [1, 2, 3, 4, 5 ,6];
-
-console.log(
-  ranks.map((e, index) => {
-    return e * 100;
-  })
-);
-
-console.log(
-  ranks.forEach((e, index) => {
-    return e * 100;
-  })
-);
-
-let newmaparr = ranks.map((e, index) => {
-    console.log(e)
-    if(e%2===0)
-        return e
+let concert = false
+let attendConcert = new Promise(function(resolve, reject) {
+    setTimeout(() => {
+        if(concert){
+            resolve("BOB ATTENDED THE CONCERT")
+        }
+        else {
+            reject("BOB FAILED TO ATTEND THE CONCERT")
+        }
+    },2000)
 })
-console.log(newmaparr);
+console.log(attendConcert)
 
-
-let newfilterarr = ranks.filter((e, index) => {
-    console.log(e)
-    if(e%2===0)
-        return e
-})
-console.log(newfilterarr);
+// attendConcert.then((data)=> console.log(data)) //we can use any variable inside this
+// attendConcert.catch((error)=> console.log(error))  
+attendConcert.then((data)=> console.log(data)).catch((error)=>console.log(error))   //will not show uncaught error
+// for true ---->then
+//
+console.log("Hi, I won't wait for anyone")  //will be printed instantaneously , will not wait for 2 sec just like above statement
