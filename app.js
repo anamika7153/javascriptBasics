016f36bda4db6abffa325482af42eea664e00272
anamika7153 <aanyayadav419@gmail.com>
2021-09-11 19:50:54 +0530

    try and catch

--- app.js
+++ app.js
@@ -9,11 +9,29 @@ let attendConcert = new Promise(function(resolve, reject) {
         }
     },2000)
 })
-console.log(attendConcert)
 
-// attendConcert.then((data)=> console.log(data)) //we can use any variable inside this
-// attendConcert.catch((error)=> console.log(error))  
-attendConcert.then((data)=> console.log(data)).catch((error)=>console.log(error))   //will not show uncaught error
-// for true ---->then
-//
+
+// async function afunc() {
+//     try {
+//     let result = await attendConcert;   //cant be used outside async function
+//     console.log(result)
+
+//     }
+//     catch(e) {
+//     console.log(e)
+
+//     }
+// }
+let asyfuncname = async() => {
+    try {
+    let result = await attendConcert;   //cant be used outside async function
+    console.log(result)
+
+    }
+    catch(e) {
+    console.log(e)
+
+    }
+}
+console.log("afunc = ",asyfuncname())
 console.log("Hi, I won't wait for anyone")  //will be printed instantaneously , will not wait for 2 sec just like above statement
