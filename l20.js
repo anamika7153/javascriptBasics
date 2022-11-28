edad6cfba51e97d9596295f1c1e63a97b9f49a3f
Anamika Yadav <aanyayadav419@gmail.com>
2022-11-28 19:39:29 +0530

    map basics

---
+++ l20.js
@@ -0,0 +1,15 @@
+console.log("Map and Promise");
+
+// Map doesn't manipulate the same array unlike forEach. It updates and return a new array.
+let ranks = [1, 2, 3];
+console.log(ranks.map((e,ind) => {
+    console.log((e+1),ind); // it will return new array with elements 2,3,4 and the indices
+    return(e*100); // It will return 100, 200, 300 as the new array  and 'ranks' has not been changed
+}))
+
+console.log("forEach")
+//forEach doesn't return a new array, returns "undefined".
+console.log(ranks.forEach((e,index) => {
+    return(e*100); //as forEach doesn't return anything hence the output will be undefined
+}))
+
